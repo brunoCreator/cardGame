fd967de95db842e8c893d265d876745859f5a0db
brunoCreator <brunoCreator@users.noreply.github.com>
2021-01-10 13:01:47 -0300

    deck player

--- src/main/java/Modelo/Deck.java
+++ src/main/java/Modelo/Deck.java
@@ -10,5 +10,39 @@ package Modelo;
  * @author rotch
  */
 public class Deck {
+    private int id;
+    private String nome;
+    Card[] cards;
+
+    public Deck(int id, String nome, Card[] cards) {
+        this.id = id;
+        this.nome = nome;
+        this.cards = cards;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Card[] getCards() {
+        return cards;
+    }
+
+    public void setCards(Card[] cards) {
+        this.cards = cards;
+    }
+    
     
 }
--- src/main/java/Modelo/Player.java
+++ src/main/java/Modelo/Player.java
@@ -10,5 +10,37 @@ package Modelo;
  * @author rotch
  */
 public class Player {
-    
+    private int id;
+    private String nome;
+    private String senha;
+
+    public Player(int id, String nome, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
