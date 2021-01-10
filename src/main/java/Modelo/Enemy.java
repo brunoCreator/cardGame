e5f0d07d7b214b66f3a84f457803de7d7a129553
brunoCreator <brunoCreator@users.noreply.github.com>
2021-01-10 12:24:42 -0300

    Update Enemy.java

--- src/main/java/Modelo/Enemy.java
+++ src/main/java/Modelo/Enemy.java
@@ -10,5 +10,57 @@ package Modelo;
  * @author rotch
  */
 public class Enemy {
-    
+    private int id;
+    private String nome;
+    private String historia;
+    private int dificuldade;
+    private Deck deck;
+
+    public Enemy(int id, String nome, String historia, int dificuldade, Deck deck) {
+        this.id = id;
+        this.nome = nome;
+        this.historia = historia;
+        this.dificuldade = dificuldade;
+        this.deck = deck;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+
+    public int getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(int dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
 }
