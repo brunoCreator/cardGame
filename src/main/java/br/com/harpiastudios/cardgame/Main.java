7dbcd4e9ac3c052c8ac501cbeaa499b1d1b5f2a9
brunoCreator <brunoCreator@users.noreply.github.com>
2021-01-11 20:29:25 -0300

    look and feel

--- src/main/java/br/com/harpiastudios/cardgame/Main.java
+++ src/main/java/br/com/harpiastudios/cardgame/Main.java
@@ -12,7 +12,27 @@ import br.com.harpiastudios.cardgame.view.MenuView;
  * @author Diego
  */
 public class Main {
+    
+
     public static void main(String[] args){
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
         MenuView view = new MenuView();
         view.setVisible(true);
     }
